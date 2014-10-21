7f527043db6bb4de25eb2f68db53b5696494e8b4
Anders Elo <anders@kaerus.com>
2014-10-21 12:37:21 +0200

    consider all whitespace

---
+++ test/fixtures/descriptions.js
@@ -0,0 +1,14 @@
+/**
+ * Function description 
+ *
+ * @param {Function} method	a method
+ * @param {Array} list  	a list
+ * @param {Object} object 	an object
+ * @throws {Error} error	on Error
+ * @return {Boolean} 		true if ok
+ * @api public
+ */
+function aFunction(a,b,c){
+    return true;
+}
+
